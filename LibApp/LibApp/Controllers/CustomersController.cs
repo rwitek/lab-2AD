292cf21a1dd05885ee5248b8f1b5d3173db01bcc
rwitek <rafal.witek@microsoft.wsei.edu.pl>
2023-10-28 10:40:14 +0200

    Adding CustomersController, getCustomer, and details

---
+++ LibApp/LibApp/Controllers/CustomersController.cs
@@ -0,0 +1,36 @@
+﻿using LibApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibApp.Controllers
+{
+    public class CustomersController
+    {
+
+
+
+
+        public IActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+            {
+                return new NotFoundResult();
+            }
+
+            return new OkObjectResult(customer);
+        }
+
+
+
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer { Id = 1, Name = "John Smith" },
+                new Customer { Id = 2, Name = "Mary Williams" }
+            };
+        }   
+    }
+}
