5eb3843ddb780824d766c344be7724500b5da67f
rwitek <rafal.witek@microsoft.wsei.edu.pl>
2023-12-09 10:40:23 +0100

    Added form to customer, and edit currently customers in page

--- LibApp/Models/Customer.cs
+++ LibApp/Models/Customer.cs
@@ -8,8 +8,12 @@ namespace LibApp.Models
         [Required]
         [StringLength(100)]
         public string Name { get; set; }
+        [Display(Name = "Date of Birth")]
         public DateTime? Birthdate { get; set; }
+        [Display(Name = "Membership Type")]
+        public int MembershipTypeId { get; set; }
         public MembershipType MembershipType { get; set; }
+
         public bool HasNewsletterSubscribed { get; set; }
         public Customer() 
         { 
