5da44abf9a5d31b3bdaf4a072f3dbfd684255eb7
rwitek <rafal.witek@microsoft.wsei.edu.pl>
2024-01-13 11:25:26 +0100

    Added Anti forgery token validation, API, automapper to Customer Controller, and solved issue with CreatedAtRoute()

---
+++ LibApp/Controllers/Api/CustomersController.cs
@@ -0,0 +1,94 @@
+﻿using LibApp.Data;
+using LibApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using AutoMapper;
+using LibApp.Dtos;
+
+namespace LibApp.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        public CustomersController(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        // GET /api/customers
+        [HttpGet]
+        public IActionResult GetCustomers()
+        {
+            return Ok(_context.Customers.ToList().Select(_mapper.Map<Customer, CustomerDto>));
+        }
+
+        // GET /api/customers/{id}
+        [HttpGet("{id}", Name = "GetCustomer")]
+        public IActionResult GetCustomer(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer == null) 
+            {
+                return NotFound();
+            }
+
+            return Ok(_mapper.Map<CustomerDto>(customer));
+        }
+
+        // POST /api/customers
+        [HttpPost]
+        public IActionResult CreateCustomer(CustomerDto customerDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            _context.Customers.Add(_mapper.Map<Customer>(customerDto));
+            _context.SaveChanges();
+            return CreatedAtRoute("GetCustomer", new { id = customerDto.Id }, customerDto);
+        }
+
+        // PUT /api/customers/{id}
+        [HttpPut("{id}")]
+        public IActionResult UpdateCustomer(int id, CustomerDto customerDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customerInDb == null)
+            {
+                return NotFound();
+            }
+
+            _mapper.Map(customerDto, customerInDb);
+
+            return Ok(customerDto);
+        }
+
+        // DELETE /api/customers/{id}
+        [HttpDelete("{id}")]
+        public IActionResult DeleteCustomer(int id)
+        {
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customerInDb == null)
+            {
+                return NotFound();
+            }
+
+            _context.Customers.Remove(customerInDb);
+            _context.SaveChanges();
+
+            return Ok(customerInDb);
+        }
+
+        private ApplicationDbContext _context;
+        private IMapper _mapper;
+    }
+}
--- LibApp/Controllers/CustomersController.cs
+++ LibApp/Controllers/CustomersController.cs
@@ -49,9 +49,31 @@ namespace LibApp.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(Customer customer)
+        [ValidateAntiForgeryToken]
+        public IActionResult Save(Customer customer)
         {
-            _context.Customers.Add(customer);
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new NewCustomerViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+                return View("CustomerForm", viewModel);
+            }
+            if (customer.Id == 0)
+            {
+                _context.Customers.Add(customer);
+            }
+            else
+            {
+                var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == customer.Id);
+                customerInDb.Name = customer.Name;
+                customerInDb.Birthdate = customer.Birthdate;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.HasNewsletterSubscribed = customer.HasNewsletterSubscribed;
+            }
+
             _context.SaveChanges();
             return RedirectToAction("Index", "Customers");
         }
